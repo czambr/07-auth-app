f648bb6feffc67f65f8a2fc12e8720da523c6497
Carlos Zambrano <>
2026-04-24 17:32:37 -0500

    feat: add layout and page components, implement theme provider, and include favicon

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,7 +1,7 @@
-import { Geist, Geist_Mono, Inter } from "next/font/google"
+import { Geist_Mono, Inter } from "next/font/google"
 
 import "@workspace/ui/globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "@/src/components/theme-provider"
 import { cn } from "@workspace/ui/lib/utils";
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'})
