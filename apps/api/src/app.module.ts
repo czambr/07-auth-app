bc01b0109809ed83c9b2f959f59bb0211a26201b
Carlos Zambrano <>
2026-04-25 15:26:01 -0500

    fix: correct Redis URL reference in app module and ensure it's included in docker-compose

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from "@nestjs/common"
 import { CacheModule } from "@nestjs/cache-manager"
 import KeyvRedis from "@keyv/redis"
 
+import { envs } from "./config"
 import { PrismaModule } from "./modules/prisma/prisma.module"
-import { env } from "process"
 
 @Module({
   imports: [
@@ -12,7 +12,7 @@ import { env } from "process"
       useFactory: () => {
         return {
           ttl: 5000,
-          stores: [new KeyvRedis(env.REDIS_URL)],
+          stores: [new KeyvRedis(envs.REDIS_URL)],
         }
       },
     }),
