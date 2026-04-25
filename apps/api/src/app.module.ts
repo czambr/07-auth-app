fa59a4df2da79027bf6b9d10b0b7ae935ac4e0c5
Carlos Zambrano <>
2026-04-25 15:13:54 -0500

    feat: integrate PostgreSQL with Prisma, add session and user models, and configure environment variables

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,24 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common"
+import { CacheModule } from "@nestjs/cache-manager"
+import KeyvRedis from "@keyv/redis"
+
+import { PrismaModule } from "./modules/prisma/prisma.module"
+import { env } from "process"
 
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.registerAsync({
+      isGlobal: true,
+      useFactory: () => {
+        return {
+          ttl: 5000,
+          stores: [new KeyvRedis(env.REDIS_URL)],
+        }
+      },
+    }),
+
+    PrismaModule,
+  ],
   controllers: [],
   providers: [],
 })
